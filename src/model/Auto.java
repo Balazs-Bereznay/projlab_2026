5449d544d344f440751e127c02759de8be351172
whoami <berbalaf.45@gmail.com>
2026-05-01 11:10:42 +0200

    Auto fel kesz, meg hianyzik a dupla id-s ProtoEntitas metodus megvalositasa

--- src/model/Auto.java
+++ src/model/Auto.java
@@ -1,95 +1,218 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
- * A rendszer által irányított jármű, amely önállóan keres útvonalat.
+ * A rendszer altal iranyitott kozlekedo jarmuvet megvalosito osztaly.
+ *
+ * <p>Az Auto felel az automatikusan mozgo auto kezdopontjanak, celpontjanak
+ * es uton toltott idejenek nyilvantartasaert. A Jarmu leszarmazottjakent
+ * hasznalja a kozos mozgasi es allapotkezelesi muveleteket, utvonalat pedig
+ * a rendszeriranyitott mukodeshez tartozo utvonalkereses alapjan kap.</p>
  */
-public class Auto extends Jarmu implements RendszerIranyitott {
+public class Auto extends Jarmu implements RendszerIranyitott, ProtoEntitas {
+
+    /**
+     * Az auto kiindulasi csomopontja.
+     */
     private Csomopont kezdopont;
+
+    /**
+     * Az auto celkent hasznalt csomopontja.
+     */
     private Csomopont celpont;
+
+    /**
+     * Az auto altal uton toltott ido.
+     */
     private int utonToltottIdo;
 
-    public Auto() {
-        super();
-        this.kezdopont = null;
-        this.celpont = null;
-        this.utonToltottIdo = 0;
-    }
-
-    public Auto(Csomopont kezdopont, Csomopont celpont, int sebesseg, Utegyseg utegyseg, int tapadas) {
-        super(sebesseg, utegyseg, tapadas);
-        this.kezdopont = kezdopont;
-        this.celpont = celpont;
-        this.utonToltottIdo = 0;
-    }
-
     /**
-     * Megtervezi az autó útvonalát a kezdőpont és a célpont között.
-     * @param kezdopont Az indulási csomópont.
-     * @param vegpont Az érkezési csomópont.
+     * Meghatarozza a kezdopont es celpont alapjan az NPC auto legrovidebb utvonalat.
+     *
+     * <p>Ha a terkep vagy valamelyik vegpont nem all rendelkezesre, akkor ures
+     * utvonalat allit be es ad vissza. Egyebkent a terkep utvonaltervezo
+     * metodusat hasznalja, az eredmenyt pedig az orokolt kijeloltUtvonal
+     * attributumban is eltarolja.</p>
+     *
+     * @param terkep a palyat es az uthalozatot tartalmazo terkep
+     * @return az auto altal kovetendo utak listaja
      */
-    public void utvonalTervezes(Csomopont kezdopont, Csomopont vegpont) {
-        this.kezdopont = kezdopont;
-        this.celpont = vegpont;
+    public List<Ut> utvonalKereses(Terkep terkep) {
+        if (terkep == null || kezdopont == null || celpont == null) {
+            this.kijeloltUtvonal = new ArrayList<>();
+            return this.kijeloltUtvonal;
+        }
 
-        System.out.println("Auto utvonalTervezes() meghivva.");
-        this.kijeloltUtvonal = new ArrayList<>();
+        this.kijeloltUtvonal = new ArrayList<>(terkep.utvonalTervezes(kezdopont, celpont));
+        return this.kijeloltUtvonal;
     }
 
     /**
-     * Útvonalat keres az autó számára a térkép alapján.
-     * @param terkep A térkép, amelyen az útvonal keresése történik.
+     * A RendszerIranyitott interfesz utvonalkeresesi muvelete.
+     *
+     * <p>Az Auto reszletes metodustervben szereplo utvonalKereses metodusra
+     * tovabbit, mert az adja vissza es tarolja el az auto utvonalat.</p>
+     *
+     * @param terkep a palyat es az uthalozatot tartalmazo terkep
      */
     @Override
     public void utvonalKeres(Terkep terkep) {
-        System.out.println("Auto utvonalKeres(Terkep) meghivva.");
-        this.kijeloltUtvonal = new ArrayList<>();
+        utvonalKereses(terkep);
     }
 
     /**
-     * Az autó egy lépését modellezi.
-     * Sikeres lépés esetén nő az úton töltött idő.
+     * Feldolgozza az autora erkezo prototipus-parancsokat.
+     *
+     * <p>A prototipus aktualis parancselosztoja a ProtoEntitas interfeszen
+     * keresztul adja at az entitasokra vonatkozo muveleteket. Az Auto a move
+     * parancsnal az orokolt mozgasi muveleteket hasznalja. Set parancsnal az
+     * Auto sajat es a Jarmu osztalybol orokolt, szovegbol biztonsagosan
+     * atalakithato attributumait kezeli. A referencia tipusu attributumokat ez
+     * a ket parameteres hivas nem allitja.</p>
+     *
+     * @param parancs a feldolgozando parancs neve
+     * @param args a parancs tovabbi parameterei
      */
     @Override
-    public void lep() {
-        super.lep();
-        if (!baleset && !elakadt) {
-            utonToltottIdo++;
-            System.out.println("Auto uton toltott ideje novelve: " + utonToltottIdo);
+    public void parancsFeldolgoz(String parancs, List<String> args) {
+        if (parancs == null || args == null) {
+            return;
+        }
+
+        switch (parancs) {
+            case "move":
+                if (args.isEmpty()) {
+                    return;
+                }
+
+                String irany = args.get(0);
+                if ("-f".equalsIgnoreCase(irany) || "forward".equalsIgnoreCase(irany)) {
+                    lep();
+                } else {
+                    savValtas(irany);
+                }
+                break;
+            case "set":
+                if (args.size() < 2) {
+                    return;
+                }
+
+                String property = args.get(0);
+                String value = args.get(1);
+                switch (property) {
+                    case "sebesseg":
+                        try {
+                            setSebesseg(Integer.parseInt(value));
+                        } catch (NumberFormatException ignored) {
+                            return;
+                        }
+                        break;
+                    case "tapadas":
+                        try {
+                            setTapadas(Integer.parseInt(value));
+                        } catch (NumberFormatException ignored) {
+                            return;
+                        }
+                        break;
+                    case "utonToltottIdo":
+                        try {
+                            setUtonToltottIdo(Integer.parseInt(value));
+                        } catch (NumberFormatException ignored) {
+                            return;
+                        }
+                        break;
+                    case "elakadt":
+                        if ("true".equalsIgnoreCase(value) || "false".equalsIgnoreCase(value)) {
+                            this.elakadt = Boolean.parseBoolean(value);
+                        }
+                        break;
+                    case "baleset":
+                        if ("true".equalsIgnoreCase(value) || "false".equalsIgnoreCase(value)) {
+                            this.baleset = Boolean.parseBoolean(value);
+                        }
+                        break;
+                    case "megcsuszott":
+                        if ("true".equalsIgnoreCase(value) || "false".equalsIgnoreCase(value)) {
+                            this.megcsuszott = Boolean.parseBoolean(value);
+                        }
+                        break;
+                    case "kijeloltUtvonal":
+                        if ("empty".equalsIgnoreCase(value)
+                                || "{}".equals(value)) {
+                            this.kijeloltUtvonal = new ArrayList<>();
+                        }
+                        break;
+                    case "kezdopont":
+                    case "celpont":
+                    case "utegyseg":
+                    case "nyilvantarto":
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case "clean":
+            case "add_condition":
+            case "purchase":
+                break;
+            default:
+                break;
         }
     }
 
+     /**
+     * Visszaadja az auto kezdopontjat.
+     *
+     * @return az auto kiindulasi csomopontja
+     */
     public Csomopont getKezdopont() {
         return kezdopont;
     }
 
+    /**
+     * Beallitja az auto kezdopontjat.
+     *
+     * @param kezdopont az auto uj kiindulasi csomopontja
+     */
     public void setKezdopont(Csomopont kezdopont) {
         this.kezdopont = kezdopont;
     }
 
+    /**
+     * Visszaadja az auto celpontjat.
+     *
+     * @return az auto celkent hasznalt csomopontja
+     */
     public Csomopont getCelpont() {
         return celpont;
     }
 
+    /**
+     * Beallitja az auto celpontjat.
+     *
+     * @param celpont az auto uj celpontja
+     */
     public void setCelpont(Csomopont celpont) {
         this.celpont = celpont;
     }
 
+    /**
+     * Visszaadja az auto uton toltott idejet.
+     *
+     * @return az uton toltott ido
+     */
     public int getUtonToltottIdo() {
         return utonToltottIdo;
     }
 
+    /**
+     * Beallitja az auto uton toltott idejet.
+     *
+     * @param utonToltottIdo az uj uton toltott ido
+     */
     public void setUtonToltottIdo(int utonToltottIdo) {
         this.utonToltottIdo = utonToltottIdo;
-    }
-
-    @Override
-    public String toString() {
-        return super.toString()
-                + " Auto{kezdopont=" + kezdopont
-                + ", celpont=" + celpont
-                + ", utonToltottIdo=" + utonToltottIdo
-                + "}";
     }
 }
