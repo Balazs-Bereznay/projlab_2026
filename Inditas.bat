58ec4f7126dd0a21a3f95aae492e2e9a515f86ef
qwert33e1 <salihelfrich@gmail.com>
2026-07-31 11:46:37 +0200

    inditas script

--- Inditas.bat
+++ Inditas.bat
@@ -1,2 +1,2 @@
-@echo off
-start "" javaw -jar "%~dp0target\projlab.jar"
+javac -encoding UTF-8 -d target/classes src/model/*.java src/common/*.java src/controller/*.java src/view/*.java
+java -cp target/classes controller.JatekIndito
